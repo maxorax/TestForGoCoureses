3fc4ec3c19387d99628c7ea5bafb7d93e8b2a05f
Maxim Orlov <morlov@65apps.com>
2022-12-09 13:51:40 +0400

    Done

--- TestForGoCourses/main.swift
+++ TestForGoCourses/main.swift
@@ -7,5 +7,16 @@
 
 import Foundation
 
-print("Hello, World!")
+for number in 1...100 {
+    if number % 15 == 0 {
+        print("FizzBuzz")
+    } else if number % 3 == 0 {
+        print("Fizz")
+
+    } else if number % 5 == 0 {
+        print("Buzz")
+    } else {
+        print(number)
+    }
+}
 
